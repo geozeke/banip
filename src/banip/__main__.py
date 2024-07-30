8f73e36d3af3691ca9be8f78573cd9e90e619b1f
Peter Nardi <pete@nardi.com>
2024-07-30 14:08:47 -0400

    Cleanup (#12)
    * Variable name cleanup.
    
    * Implemented more descriptive function names.
    
    * Variable tuning.
    
    * Refactor argument parsing.
    
    * Code tuning.
    
    * Added version information to command line.
    
    * Fixed option ordering.
    
    * Improved help.
    
    * Cleanup file permissions.
    
    * Dependency Updates.
    
    * Reduce locations of version number.
    
    * Variable name cleanup.

--- src/banip/__main__.py
+++ src/banip/__main__.py
@@ -4,19 +4,22 @@
 
 import argparse
 import importlib
+import sys
 from pathlib import Path
 from types import ModuleType
 
+from banip.constants import APP_NAME
 from banip.constants import ARG_PARSERS_BASE
-from banip.constants import CUSTOM_ARG_PARSERS
+from banip.constants import ARG_PARSERS_CUSTOM
 from banip.constants import CUSTOM_CODE
+from banip.constants import VERSION
 from banip.utilities import wrap_tight
 
 # ======================================================================
 
 
-def collect_modules(start: Path) -> list[str]:
-    """Collect the names of all modules to import.
+def collect_parsers(start: Path) -> list[str]:
+    """Collect the module names of all argument parsers to import.
 
     Parameters
     ----------
@@ -26,43 +29,17 @@ def collect_modules(start: Path) -> list[str]:
     Returns
     -------
     list[str]
-        A list of module names.
+        A list of argument parser module names.
     """
-    module_names: list[str] = []
+    parser_names: list[str] = []
     for p in start.iterdir():
         if p.is_file() and p.name != "__init__.py":
             if "plugins" in str(p):
                 prefix = "plugins.parsers"
             else:
                 prefix = "parsers"
-            module_names.append(f"{prefix}.{p.stem}")
-    return module_names
-
-
-# ======================================================================
-
-
-def load_custom_module(cmd: str) -> ModuleType | None:
-    """Given the name of a command, return the associated module.
-
-    By design, the code associated with a given command must have the
-    same name as the command itself.
-
-    Parameters
-    ----------
-    cmd : str
-        The name of a banip command
-
-    Returns
-    -------
-    ModuleType | None
-        This will be a pointer to the imported python module.
-    """
-    for p in CUSTOM_CODE.iterdir():
-        if p.is_file():
-            if p.stem == cmd:
-                return importlib.import_module(f"plugins.code.{p.stem}")
-    return None
+            parser_names.append(f"{prefix}.{p.stem}")
+    return parser_names
 
 
 # ======================================================================
@@ -71,43 +48,61 @@ def load_custom_module(cmd: str) -> ModuleType | None:
 def main() -> None:
     """Get user input and build the list of banned IP addresses."""
     msg = """Generate and query IP blacklists for use with proxy servers
-    (like HAProxy). For help on any command, run: "banip {command} -h".
-    Please review the README file at https://github.com/geozeke/banip
-    for detailed instructions on setting up banip."""
-    epi = "Version: 1.0.0"
+    (like HAProxy). Please review the README file at
+    https://github.com/geozeke/banip for detailed instructions on
+    setting up banip."""
+    epi = f"Version: {VERSION}"
     parser = argparse.ArgumentParser(
         description=msg,
         epilog=epi,
     )
-    subparsers = parser.add_subparsers(title="commands", dest="cmd")
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{APP_NAME} {VERSION}",
+    )
+    msg = "For help on any command below, run: banip {command} -h."
+    subparsers = parser.add_subparsers(
+        title="commands",
+        dest="cmd",
+        description=msg,
+    )
 
-    # Dynamically load argument subparsers.
+    # Dynamically load argument subparsers and process command line
+    # arguments.
 
-    module_names: list[str] = []
+    parser_names: list[str] = []
     mod: ModuleType | None = None
-    module_names = collect_modules(ARG_PARSERS_BASE)
-    module_names += collect_modules(CUSTOM_ARG_PARSERS)
-    for mod_name in module_names:
-        mod = importlib.import_module(mod_name)
-        mod.load_command_args(subparsers)
-
+    parser_names = collect_parsers(ARG_PARSERS_BASE)
+    parser_names += collect_parsers(ARG_PARSERS_CUSTOM)
+    for p_name in parser_names:
+        parser_code = importlib.import_module(p_name)
+        parser_code.load_command_args(subparsers)
     args = parser.parse_args()
-    match (args.cmd):
-        case "build":
-            mod = importlib.import_module("banip.build")
-        case "check":
-            mod = importlib.import_module("banip.check")
-        case _:
-            if args.cmd:
-                if not (mod := load_custom_module(args.cmd)):
-                    msg = f"""Code for a given command must have the
-                    same name as the command itself. Make sure you have
-                    a program file called \"{args.cmd}.py\" in
-                    {CUSTOM_CODE}/"""
-                    print(wrap_tight(msg))
-                    mod = importlib.import_module("banip.null")
-            else:
-                mod = importlib.import_module("banip.null")
+
+    # Run the selected command. Python's argparse module guarantees that
+    # we'll get either: (1) a valid command (base or custom) or (2) no
+    # command at all (args.cmd == None). Given that, we can easily
+    # determine if the entered command is base or custom, based on its
+    # companion in the list of argument parser names. We then adjust the
+    # prefix based on that.
+
+    if args.cmd:
+        if f"parsers.{args.cmd}_args" in parser_names:
+            prefix = f"{APP_NAME}"
+        else:
+            prefix = "plugins.code"
+        try:
+            mod = importlib.import_module(f"{prefix}.{args.cmd}")
+        except ModuleNotFoundError:
+            msg = f"""Code for a custom command must have the same
+            filename as the command itself. Make sure you have a program
+            file called \"{args.cmd}.py\" in {CUSTOM_CODE}/"""
+            print(wrap_tight(msg))
+            sys.exit(1)
+    else:
+        mod = importlib.import_module(f"{APP_NAME}.null")
     mod.task_runner(args)
 
     return
--- src/banip/constants.py
+++ src/banip/constants.py
@@ -8,20 +8,22 @@ from pathlib import Path
 
 HOME = Path(__file__).parents[2]
 
-ARG_PARSERS_BASE = HOME / "src/parsers"
-BANNED_IPS = HOME / "data/ipsum.txt"
-COUNTRY_NETS = HOME / "data/haproxy_geo_ip.txt"
-CUSTOM_ARG_PARSERS = HOME / "src/plugins/parsers"
-CUSTOM_BLACKLIST = HOME / "data/custom_blacklist.txt"
-CUSTOM_CODE = HOME / "src/plugins/code"
-CUSTOM_WHITELIST = HOME / "data/custom_whitelist.txt"
-GEOLITE_4 = HOME / "data/geolite/GeoLite2-Country-Blocks-IPv4.csv"
-GEOLITE_6 = HOME / "data/geolite/GeoLite2-Country-Blocks-IPv6.csv"
-GEOLITE_LOC = HOME / "data/geolite/GeoLite2-Country-Locations-en.csv"
+APP_NAME = "banip"
+ARG_PARSERS_BASE = HOME / "src" / "parsers"
+ARG_PARSERS_CUSTOM = HOME / "src" / "plugins" / "parsers"
+BANNED_IPS = HOME / "data" / "ipsum.txt"
+COUNTRY_NETS = HOME / "data" / "haproxy_geo_ip.txt"
+CUSTOM_BLACKLIST = HOME / "data" / "custom_blacklist.txt"
+CUSTOM_CODE = HOME / "src" / "plugins" / "code"
+CUSTOM_WHITELIST = HOME / "data" / "custom_whitelist.txt"
+GEOLITE_4 = HOME / "data" / "geolite" / "GeoLite2-Country-Blocks-IPv4.csv"
+GEOLITE_6 = HOME / "data" / "geolite" / "GeoLite2-Country-Blocks-IPv6.csv"
+GEOLITE_LOC = HOME / "data" / "geolite" / "GeoLite2-Country-Locations-en.csv"
 IPS = [IPv4Address, IPv6Address]
 NETS = [IPv4Network, IPv6Network]
-RENDERED_BLACKLIST = HOME / "data/ip_blacklist.txt"
-TARGETS = HOME / "data/targets.txt"
+RENDERED_BLACKLIST = HOME / "data" / "ip_blacklist.txt"
+TARGETS = HOME / "data" / "targets.txt"
+VERSION = "1.0.1"
 
 # Padding for pretty printing.
 PAD = 6

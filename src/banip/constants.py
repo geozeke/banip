64ac6e8c6f01456068555fb4f05f327b8a683bb9
Peter Nardi <geozeke@gmail.com>
2025-01-13 07:09:56 -0500

    Testing.

--- src/banip/constants.py
+++ src/banip/constants.py
@@ -13,7 +13,7 @@ DATA = HOME / ".banip"
 
 APP_NAME = "banip"
 ARG_PARSERS_BASE = BASE / "parsers"
-ARG_PARSERS_CUSTOM = DATA / "plugins"
+ARG_PARSERS_CUSTOM = DATA / "plugins" / "parsers"
 CUSTOM_CODE = DATA / "plugins" / "code"
 COUNTRY_NETS_TXT = DATA / "haproxy_geo_ip.txt"
 COUNTRY_NETS_DICT = DATA / "haproxy_geo_ip_dict.bin"

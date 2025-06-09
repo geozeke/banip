36de10a85bd043903a498e61e38850b9ca1cc5d2
Peter Nardi <geozeke@gmail.com>
2025-06-09 11:31:04 -0400

    Refactored argument types.

--- src/banip/parsers/build_args.py
+++ src/banip/parsers/build_args.py
@@ -1,75 +1,12 @@
 """Argument parser for build command."""
 
-from argparse import ArgumentTypeError
 from argparse import FileType
 from argparse import _SubParsersAction
 
+from banip.argument_types import compact_type
+from banip.argument_types import threshold_type
+
 COMMAND_NAME = "build"
-
-
-def threshold_type(x: str) -> int:
-    """Validate the threshold input.
-
-    Parameters
-    ----------
-    x : str
-        User input for the threshold option.
-
-    Returns
-    -------
-    int
-        The validated user input.
-
-    Raises
-    ------
-    argparse.ArgumentTypeError
-        If the user input is not an integer type
-    argparse.ArgumentTypeError
-        If the user input is not within the acceptable range [1,10]
-    """
-    try:
-        x_int = int(x)
-    except ValueError:
-        raise ArgumentTypeError("Threshold must be an integer")
-
-    if x_int not in range(1, 11):
-        raise ArgumentTypeError("Threshold must be between 1 and 10")
-
-    return x_int
-
-
-# ======================================================================
-
-
-def compact_type(x: str) -> int:
-    """Validate the compact input.
-
-    Parameters
-    ----------
-    x : str
-        User input for the compact option.
-
-    Returns
-    -------
-    int
-        The validated user input.
-
-    Raises
-    ------
-    argparse.ArgumentTypeError
-        If the user input is not an integer type
-    argparse.ArgumentTypeError
-        If the user input is not within the acceptable range [1,255]
-    """
-    try:
-        x_int = int(x)
-    except ValueError:
-        raise ArgumentTypeError("Compact must be an integer")
-
-    if x_int not in range(1, 256):
-        raise ArgumentTypeError("Compact must be between 1 and 255")
-
-    return x_int
 
 
 # ======================================================================
--- src/banip/parsers/patch_args.py
+++ src/banip/parsers/patch_args.py
@@ -1,41 +1,11 @@
 """Argument parser for build command."""
 
-from argparse import ArgumentTypeError
 from argparse import FileType
 from argparse import _SubParsersAction
 
+from banip.argument_types import threshold_type
+
 COMMAND_NAME = "patch"
-
-
-def threshold_type(x: str) -> int:
-    """Validate the threshold input.
-
-    Parameters
-    ----------
-    x : str
-        User input for the threshold option.
-
-    Returns
-    -------
-    int
-        The validated user input.
-
-    Raises
-    ------
-    argparse.ArgumentTypeError
-        If the user input is not an integer type
-    argparse.ArgumentTypeError
-        If the user input is not within the acceptable range [1,10]
-    """
-    try:
-        x_int = int(x)
-    except ValueError:
-        raise ArgumentTypeError("Threshold must be an integer")
-
-    if x_int not in range(1, 11):
-        raise ArgumentTypeError("Threshold must be between 1 and 10")
-
-    return x_int
 
 
 # ======================================================================
